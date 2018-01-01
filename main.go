9f0855e894be205915cce7940d6d9b4208852afc
bdrozhak <bdrozhak@cisco.com>
2018-01-01 19:22:25 +0200

    add generated code

---
+++ main.go
@@ -0,0 +1,30 @@
+//go:generate goagen bootstrap -d BorysDrozhak/GOA_start/design
+
+package main
+
+import (
+	"BorysDrozhak/GOA_start/app"
+	"github.com/goadesign/goa"
+	"github.com/goadesign/goa/middleware"
+)
+
+func main() {
+	// Create service
+	service := goa.New("cellar")
+
+	// Mount middleware
+	service.Use(middleware.RequestID())
+	service.Use(middleware.LogRequest(true))
+	service.Use(middleware.ErrorHandler(service, true))
+	service.Use(middleware.Recover())
+
+	// Mount "bottle" controller
+	c := NewBottleController(service)
+	app.MountBottleController(service, c)
+
+	// Start service
+	if err := service.ListenAndServe(":8080"); err != nil {
+		service.LogError("startup", "err", err)
+	}
+
+}
